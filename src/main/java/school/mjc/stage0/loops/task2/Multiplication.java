20977b707c3144596dab231c46a6257a9ca6fc0f
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-07-28 17:49:35 +0300

    Complited

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,10 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
-        int count = 0;
-        while (count <= Math.pow(multiplyByAndToInclusive, 2)) {
-            System.out.println(count);
-            count = count + multiplyByAndToInclusive;
+        if (multiplyByAndToInclusive == 0) {
+            System.out.print("");
+        } else {
+            int i = 0;
+            while (i < Math.abs(multiplyByAndToInclusive) + 1) {
+                System.out.println(i * multiplyByAndToInclusive);
+                i++;
+            }
         }
     }
 }
